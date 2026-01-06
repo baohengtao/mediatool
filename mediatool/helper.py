7298bb888a93b3f871345a2925b0a5fff14f3641
Bao Hengtao <baohengtao@users.noreply.github.com>
2026-01-07 02:52:18 +0800

    add exiftool xmp related to helper.py

--- mediatool/helper.py
+++ mediatool/helper.py
@@ -1,10 +1,51 @@
 
 import asyncio
+import time
 from functools import wraps
 from pathlib import Path
 from typing import Iterator
 
+from exiftool import ExifToolHelper
+
 from mediatool import console
+
+et = ExifToolHelper()
+
+
+def get_xmp(img: Path, with_sound: bool = False):
+    if img.suffix == '.ts':
+        return {}
+    meta = et.get_metadata(img)[0]
+    xmp = {k: v for k, v in meta.items() if k.startswith('XMP:')
+           and k != 'XMP:XMPToolkit'}
+    if with_sound:
+        k = 'QuickTime:Information'
+        xmp[k] = meta.get(k)
+    return xmp
+
+
+def write_xmp(img: Path, tags: dict):
+    for k, v in tags.copy().items():
+        if isinstance(v, str):
+            tags[k] = v.replace('\n', '&#x0a;')
+    if not tags:
+        return
+    console.log(f'writing {tags} to {img}')
+    start_time = time.monotonic()
+    params = ['-ignoreMinorErrors', '-escapeHTML', '-overwrite_original']
+    with ExifToolHelper() as et:
+        et.set_tags(img, tags, params=params)
+    console.log(f'write meta in {time.monotonic()-start_time:.1f} seconds')
+
+
+def copy_meta(src: Path, dst: Path, with_sound=False):
+    meta = et.get_metadata(src)[0]
+    xmp = {k: v for k, v in meta.items() if k.startswith('XMP:')
+           and k not in ['XMP:XMPToolkit', 'XMP:Volume']}
+    if with_sound:
+        xmp |= {k: v for k, v in meta.items() if k in [
+            'XMP:Volume', 'QuickTime:Information']}
+    write_xmp(dst, xmp)
 
 
 def run_async(func):
