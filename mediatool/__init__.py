17a204ffab7cf50cecf8354cfe2affe32a1a54e6
Bao Hengtao <baohengtao@users.noreply.github.com>
2026-01-06 17:36:12 +0800

    add __init__.py

---
+++ mediatool/__init__.py
@@ -0,0 +1,12 @@
+from rich.console import Console
+from rich.theme import Theme
+from rich.traceback import install
+
+custom_theme = Theme({
+    "info": "dim cyan",
+    "warning": "bold bright_yellow on dark_orange",
+    "error": "bold bright_red on dark_red",
+    "notice": "bold magenta"
+})
+console = Console(theme=custom_theme, record=True, width=126)
+install(show_locals=False)
